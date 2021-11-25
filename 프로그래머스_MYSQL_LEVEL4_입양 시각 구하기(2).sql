eff2897590e707707594336132fdba96a6630206
stk346 <stk346@gmail.com>
2021-11-25 19:36:03 +0900

    SQL

---
+++ 프로그래머스_MYSQL_LEVEL4_입양 시각 구하기(2).sql
@@ -0,0 +1,11 @@
+-- 프로그래머스_MYSQL_LEVEL4_입양 시각 구하기(2)
+-- https://programmers.co.kr/learn/courses/30/lessons/59413
+
+SET @HOUR_LIST := -1;
+
+SELECT
+(@HOUR_LIST := @HOUR_LIST + 1) AS "HOUR",
+(SELECT COUNT(*) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @HOUR_LIST) AS "COUNT"
+FROM ANIMAL_OUTS
+
+WHERE @HOUR_LIST < 23
