d2232aa55f8f19aad2a825647a29ec1b73f0781b
stk346 <stk346@gmail.com>
2021-12-08 21:46:40 +0900

    SQL

---
+++ LeetCode/LeetCode_Rising Temperature.sql
@@ -0,0 +1,9 @@
+-- LeetCode: Rising Temperature
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
+;
