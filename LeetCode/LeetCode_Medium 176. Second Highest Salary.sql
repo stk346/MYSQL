6ee2b9fb6e6b8e334cd7a6b04d7253175c12a64d
stk346 <stk346@gmail.com>
2022-01-08 13:52:41 +0900

    sql

--- LeetCode/LeetCode_Medium 176. Second Highest Salary.sql
+++ LeetCode/LeetCode_Medium 176. Second Highest Salary.sql
@@ -5,3 +5,18 @@ select
         from Employee
         order by salary desc
         limit 1 offset 1), null) as "SecondHighestSalary"
+
+-- 다른 풀이 (MAx 사용)
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary)
+                FROM Employee)
+
+-- 다른 풀이 (case when 사용)
+SELECT 
+    CASE 
+    WHEN COUNT(DISTINCT(Salary)) <= 1 THEN null 
+    ELSE  (select salary from employee order by salary desc limit 1, 1)
+    END 
+AS SecondHighestSalary 
+FROM Employee 
